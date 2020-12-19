43818c921f186eaf4e368e506cb3a29a767240ab
mkhartman <hartman7@umbc.edu>
2020-12-19 18:47:21 -0500

    Add project files.

---
+++ CallBomb/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace CallBomb
+{
+    class Program
+    {
+        // Vars
+        public static string accountSid = "AC7fffb946c66c7fe5ad83f49e34feebe1";
+        public static string authToken = "f5aa944daae4664b824baa4adc61e5ba";
+        public static List<string> phoneNumbers = new List<string>( new string[] { "+19285997423" });
+        
+        public static string targetNum = "";
+                          
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===== Phone Scammer Tele-Bomb V1 =====");
+            Console.WriteLine("Enter the Target Number: ");
+            targetNum = Console.ReadLine();
+            Console.WriteLine("WARNING: Target " + targetNum + " is acquired, Press enter to confirm, otherwise quit now...");
+            Console.ReadLine();
+            Console.Clear();
+
+            TwilioClient.Init(accountSid, authToken);
+
+            int count = 1;
+            do
+            {
+                Console.WriteLine("Starting Call Batch " + count);
+                foreach(string phoneNum in phoneNumbers)
+                {
+                    Call(phoneNum);
+                    System.Threading.Thread.Sleep(1000);
+                }
+                count++;
+                System.Threading.Thread.Sleep(5000);
+
+            } while (true);
+            
+        }
+
+        static void Call(string caller)
+        {
+            try
+            {
+                var call = CallResource.Create(
+                    to: new PhoneNumber(targetNum),
+                    from: new PhoneNumber(caller),
+                    record: true,
+                    url: new Uri("http://demo.twilio.com/docs/classic.mp3")
+                    );
+
+                Console.WriteLine();
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(string.Format($"Error with number: {caller}, {ex.Message}"));
+            }
+
+        }
+    }
+}
